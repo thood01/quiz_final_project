108204cea3371aa39c9ac2d84c1ff9fc3f870860
Tiffany Price <tprice@TP-Kapor-MacBook.local>
2015-10-07 21:28:23 -0700

    lots of changes. Still need to fix percentage correct functionality.

--- final_project.py
+++ final_project.py
@@ -2,6 +2,11 @@
 #Code overview: This is an application to allow users to answer trivia questions about US state and world capitals.
 #All rights reserved.
 
+#TO DO
+# combine state and world capitals
+# give option to enter 'quit' to stop playing game but still output number right and wrong
+# show percentage correct of number did
+
 print "How well do you know the famous capitals from around the world?"
 print "With this game, you can either test your knowledge of US state capitals, capitals of world countries, or both US state capitals AND world country capitals."
 #game_option = raw_input("To test your knowledge of US state capitals, please type A. For world country capitals, please type B. For both, please type C.")
@@ -15,34 +20,41 @@ with open("state_capitals.csv") as f:
 		(key, val) = line.split(",")
 		state_capitals_d[key] = val
 
+right = 0
+wrong = 0
 def main():
-	right = 0
-	wrong = 0
 	incorrect_answers=[]
-
 while len(state_capitals_d)>0:
 	choice=random.choice(state_capitals_d.keys())
-	# correct_answer=state_capitals_d.get(choice)
-
+	correct_answer=state_capitals_d.get(choice)
 	
-	answer=raw_input("What is the capital city of",choice,"?")
+	print "What is the capital city of",choice+"?"
+	answer=raw_input("# ")
 	if answer.lower()==correct_answer.lower():
 		right += 1
 		print "Correct!\n"
 		del state_capitals_d[choice]
 	else:
+		wrong += 1
+		# incorrect_answers.append(choice)
 		print "Close, but no cigar."
-		print "The correct answer is",correct_answer
-		incorrect_answers.append(choice)
+		print "The correct answer is",correct_answer+"."
+		del state_capitals_d[choice]
+	percent_right = float(right/wrong)*100
+if percent_right <=80:
+	print "You missed",wrong,"states.\nThat's only",percent_right,"percent correct. Better luck next time."
+elif percent_wrong<=90:
+	print "You missed",wrong,"states.\nThat's pretty good!"
+else:
+	print "You only missed",wrong,"states.\nAmazing!"
+	# print "You missed",len(incorrect_answers),"states.\n"
 
-	print "You missed",len(incorrect_answers),"states.\n"
-
-if incorrect_answers:
-	print "Here are the ones that you missed:\n"
-	for each in incorrect_answers:
-		print each
-	else:
-		print "Perfect!"
+# if incorrect_answers:
+# 	print "Here are the ones that you missed:\n"
+# 	for each in incorrect_answers:
+# 		print each
+# 	else:
+		# print "Perfect!"
 
         
 # response=""
